f0a3e6ddceed33c4b3c8ea3c0e286920792a79e9
lnrw <lnrdwlkr+0@outlook.com>
2024-07-22 21:31:48 +0100

    Add button to save textures

--- scripts/main.js
+++ scripts/main.js
@@ -6,9 +6,9 @@ for (let i = 0; i < numberOfTextures; i++) {
 	const canvas = document.createElement("canvas");
 	canvas.width = 16;
 	canvas.height = 16;
-	canvas.style.width = "256px";
-	canvas.style.height = "256px";
-	document.body.appendChild(canvas);
+	canvas.classList.add("texture-canvas");
+	const container = document.querySelector(".texture-canvas-container");
+	container.appendChild(canvas);
 	const ctx = canvas.getContext("2d");
 	ctx.imageSmoothingEnabled = false;
 	for (let y = 0; y < textureSize; y++) {
@@ -28,3 +28,55 @@ for (let i = 0; i < numberOfTextures; i++) {
 	}
 
 }
+
+
+const textureNames = ["air", "grass", "dirt1", "dirt2", "stone", "bricks", "dirt3", "log", "leaves", "water", "dirt4", "dirt5", "dirt6", "dirt7", "dirt8", "dirt9"];
+const saveBtn = document.getElementById("save");
+
+saveBtn.addEventListener("click", () => {
+	const canvases = document.getElementsByClassName("texture-canvas");
+	const zip = new JSZip();
+	const promises = [];
+
+	for (let i = 0; i < canvases.length; ++i) {
+		const canvas = canvases[i];
+
+		const textureBlockIndex = Math.floor(i / 3);
+		const textureName = textureNames[textureBlockIndex];
+
+		const textureFaceIndex = i % 3;
+		let textureFace;
+
+		if (textureFaceIndex === 0) {
+			textureFace = "top";
+		} else if (textureFaceIndex === 1) {
+			textureFace = "side";
+		} else {
+			textureFace = "bottom";
+		}
+
+		const promise = new Promise((resolve) => {
+			canvas.toBlob((blob) => {
+				zip.file(`${textureName}-${textureFace}.png`, blob, {
+					binary: true
+				});
+				resolve();
+			}, "image/png");
+		});
+
+		promises.push(promise);
+	}
+
+	Promise.all(promises).then(() => {
+		zip.generateAsync({
+			type: "blob"
+		}).then((content) => {
+			const a = document.createElement("a");
+			a.href = URL.createObjectURL(content, {
+				type: "application/octet-stream"
+			});
+			a.download = "textures.zip";
+			a.click();
+		});
+	});
+});
