54657b5084d5b0c645911a9a12e2984b3f0110f9
lnrw <lnrdwlkr+0@outlook.com>
2024-07-23 22:19:36 +0100

    Add button to regenerate textures

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,43 @@
 const numberOfTextures = 16 * 3;
 const textureSize = 16;
 
-// Draw textures
-for (let i = 0; i < numberOfTextures; i++) {
-	const canvas = document.createElement("canvas");
-	canvas.width = 16;
-	canvas.height = 16;
-	canvas.classList.add("texture-canvas");
+function drawTextures() {
 	const container = document.querySelector(".texture-canvas-container");
-	container.appendChild(canvas);
-	const ctx = canvas.getContext("2d");
-	ctx.imageSmoothingEnabled = false;
-	for (let y = 0; y < textureSize; y++) {
-		for (let x = 0; x < textureSize; x++) {
-			const index = x + y * textureSize + i * textureSize * textureSize;
-			const col = texmap[index];
-			const r = (col >> 16) & 0xff;
-			const g = (col >> 8) & 0xff;
-			const b = col & 0xff;
-			if (r && g && b) {
-				ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-			} else {
-				ctx.fillStyle = "rgba(0, 0, 0, 0)";
-			}
-			ctx.fillRect(x, y, 1, 1);
-		}
+
+	// Clear canvas container
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
 	}
 
+	// Draw textures
+	for (let i = 0; i < numberOfTextures; i++) {
+		const canvas = document.createElement("canvas");
+		canvas.width = 16;
+		canvas.height = 16;
+		canvas.classList.add("texture-canvas");
+		container.appendChild(canvas);
+		const ctx = canvas.getContext("2d");
+		ctx.imageSmoothingEnabled = false;
+		for (let y = 0; y < textureSize; y++) {
+			for (let x = 0; x < textureSize; x++) {
+				const index = x + y * textureSize + i * textureSize * textureSize;
+				const col = texmap[index];
+				const r = (col >> 16) & 0xff;
+				const g = (col >> 8) & 0xff;
+				const b = col & 0xff;
+				if (r && g && b) {
+					ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+				} else {
+					ctx.fillStyle = "rgba(0, 0, 0, 0)";
+				}
+				ctx.fillRect(x, y, 1, 1);
+			}
+		}
+
+	}
 }
+
+drawTextures();
 
 
 const textureNames = ["air", "grass", "dirt1", "dirt2", "stone", "bricks", "dirt3", "log", "leaves", "water", "dirt4", "dirt5", "dirt6", "dirt7", "dirt8", "dirt9"];
@@ -79,4 +89,12 @@ saveBtn.addEventListener("click", () => {
 			a.click();
 		});
 	});
+});
+
+
+const regenBtn = document.getElementById("regenerate");
+
+regenBtn.addEventListener("click", () => {
+	generateTextures();
+	drawTextures();
 });
--- scripts/textures.js
+++ scripts/textures.js
@@ -8,92 +8,96 @@ var textures = {
 	'water': 9
 };
 
-// There are 16 different textures.
-for (var texture = 1; texture < 16; texture++) {
+function generateTextures() {
+	// There are 16 different textures.
+	for (var texture = 1; texture < 16; texture++) {
 
-	// Pick a random brightness
-	var br = 255 - ((Math.random() * 96) | 0); // (The "|0" ensures integer)
+		// Pick a random brightness
+		var br = 255 - ((Math.random() * 96) | 0); // (The "|0" ensures integer)
 
-	// The map for the texture - 3 16x16 squares.
-	// Square 1: y = [0,15] - Top of cube
-	// Square 2: y = [16,31] - Sides of cube (only 2 are ever visible)
-	// Square 3: y = [32,47] - Bottom of cube
-	// 'y' basically denotes the row of the map.
-	for (var y = 0; y < 16 * 3; y++) {
+		// The map for the texture - 3 16x16 squares.
+		// Square 1: y = [0,15] - Top of cube
+		// Square 2: y = [16,31] - Sides of cube (only 2 are ever visible)
+		// Square 3: y = [32,47] - Bottom of cube
+		// 'y' basically denotes the row of the map.
+		for (var y = 0; y < 16 * 3; y++) {
 
-		// Hence, 'x' denotes the column per row.
-		for (var x = 0; x < 16; x++) {
-			var color = 0x966C4A; //dirt brown
-			if (texture == textures['stone'])
-				color = 0x7F7F7F; // stone gray
-			if (texture != textures['stone'] || ((Math.random() * 3) | 0) == 0) {
-				br = 255 - ((Math.random() * 96) | 0);
-			}
-			// If we are in the top (16 + 5 or 6) rows of the grass topped block
-			if ((texture == textures['grass-top'] && y < (((x * x * 3 + x * 81) >> 2) & 3) + 18)) {
-				color = 0x6AAA40; // grass green
-			} else if ((texture == textures['grass-top'] && y < (((x * x * 3 + x * 81) >> 2) & 3) + 19)) {
-				br = br * 2 / 3; // reduce brightness by a third
-			}
-
-			if (texture == textures['wood']) {
-				color = 0x675231; // woody brown
-
-				// If we are in square 1 or 3
-				if (x > 0 && x < 15 &&
-					((y > 0 && y < 15) || (y > 32 && y < 47))) {
-					color = 0xBC9862; // light wood
-					// Makes concentric squares
-					var xd = (x - 7);
-					var yd = ((y & 15) - 7);
-					if (xd < 0)
-						xd = 1 - xd;
-					if (yd < 0)
-						yd = 1 - yd;
-					if (yd > xd)
-						xd = yd;
-					// fiddles with br for the light/dark sq's.
-					br = 196 - ((Math.random() * 32) | 0) + xd % 3 * 32;
-				} else if (((Math.random() * 2) | 0) == 0) {
-					// Makes the sides with the woody brown
-					br = br * (150 - (x & 1) * 100) / 100;
+			// Hence, 'x' denotes the column per row.
+			for (var x = 0; x < 16; x++) {
+				var color = 0x966C4A; //dirt brown
+				if (texture == textures['stone'])
+					color = 0x7F7F7F; // stone gray
+				if (texture != textures['stone'] || ((Math.random() * 3) | 0) == 0) {
+					br = 255 - ((Math.random() * 96) | 0);
 				}
-			}
-
-			if (texture == textures['brick']) {
-				color = 0xB53A15; // Brick red
-				// Every 8th column, but alternately offset by 4
-				// OR every 4th row
-				if ((x + (y >> 2) * 4) % 8 == 0 || y % 4 == 0) {
-					color = 0xBCAFA5; // Beige brick spacing
+				// If we are in the top (16 + 5 or 6) rows of the grass topped block
+				if ((texture == textures['grass-top'] && y < (((x * x * 3 + x * 81) >> 2) & 3) + 18)) {
+					color = 0x6AAA40; // grass green
+				} else if ((texture == textures['grass-top'] && y < (((x * x * 3 + x * 81) >> 2) & 3) + 19)) {
+					br = br * 2 / 3; // reduce brightness by a third
 				}
-			}
 
-			if (texture == textures['water']) {
-				color = 0x4040ff; // Watery blue
-			}
+				if (texture == textures['wood']) {
+					color = 0x675231; // woody brown
 
-			// If we're in the third square of the texture map,
-			// Cut down brightness by half (bottom of cubes)
-			var brr = br;
-			if (y >= 32)
-				brr /= 2;
-
-			if (texture == textures['leafy']) {
-				color = 0x50D937; // leaf green
-
-				// 50% chance of no leaf spots
-				if (((Math.random() * 2) | 0) == 0) {
-					color = 0;
-					brr = 255;
+					// If we are in square 1 or 3
+					if (x > 0 && x < 15 &&
+						((y > 0 && y < 15) || (y > 32 && y < 47))) {
+						color = 0xBC9862; // light wood
+						// Makes concentric squares
+						var xd = (x - 7);
+						var yd = ((y & 15) - 7);
+						if (xd < 0)
+							xd = 1 - xd;
+						if (yd < 0)
+							yd = 1 - yd;
+						if (yd > xd)
+							xd = yd;
+						// fiddles with br for the light/dark sq's.
+						br = 196 - ((Math.random() * 32) | 0) + xd % 3 * 32;
+					} else if (((Math.random() * 2) | 0) == 0) {
+						// Makes the sides with the woody brown
+						br = br * (150 - (x & 1) * 100) / 100;
+					}
 				}
-			}
 
-			// Some color map and putting it into the huge texture map
-			var col = (((color >> 16) & 0xff) * brr / 255) << 16 |
-				(((color >> 8) & 0xff) * brr / 255) << 8 |
-				(((color) & 0xff) * brr / 255);
-			texmap[x + y * 16 + texture * 256 * 3] = col;
-		} // End of column loop
-	} // End of row loop
-} // End of 16 textures loop
+				if (texture == textures['brick']) {
+					color = 0xB53A15; // Brick red
+					// Every 8th column, but alternately offset by 4
+					// OR every 4th row
+					if ((x + (y >> 2) * 4) % 8 == 0 || y % 4 == 0) {
+						color = 0xBCAFA5; // Beige brick spacing
+					}
+				}
+
+				if (texture == textures['water']) {
+					color = 0x4040ff; // Watery blue
+				}
+
+				// If we're in the third square of the texture map,
+				// Cut down brightness by half (bottom of cubes)
+				var brr = br;
+				if (y >= 32)
+					brr /= 2;
+
+				if (texture == textures['leafy']) {
+					color = 0x50D937; // leaf green
+
+					// 50% chance of no leaf spots
+					if (((Math.random() * 2) | 0) == 0) {
+						color = 0;
+						brr = 255;
+					}
+				}
+
+				// Some color map and putting it into the huge texture map
+				var col = (((color >> 16) & 0xff) * brr / 255) << 16 |
+					(((color >> 8) & 0xff) * brr / 255) << 8 |
+					(((color) & 0xff) * brr / 255);
+				texmap[x + y * 16 + texture * 256 * 3] = col;
+			} // End of column loop
+		} // End of row loop
+	} // End of 16 textures loop
+}
+
+generateTextures();
